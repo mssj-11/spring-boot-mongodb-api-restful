5ffbdca301c254af8488a54dd781c6ad271425c3
Mike <80673026+mssj-11@users.noreply.github.com>
2023-04-12 18:39:01 -0600

    Spring Boot - MongoDB - API RestFul

---
+++ src/main/java/com/mss/springboot/web/app/SpringBootMongodbApiRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.mss.springboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbApiRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodbApiRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mss/springboot/web/app/SpringBootMongodbApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mss.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodbApiRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
